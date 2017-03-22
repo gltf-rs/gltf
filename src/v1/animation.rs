6d71a8413b5d03cdd1ac9360685123440a836ef8
Matthew Johnston <warmwaffles@gmail.com>
2017-03-22 00:41:13 -0500

    Added basic test to v1::animation

--- src/v1/animation.rs
+++ src/v1/animation.rs
@@ -90,3 +90,72 @@ pub struct Animation {
     // TODO: extension
     // TODO: extras
 }
+
+#[cfg(test)]
+mod test {
+    extern crate serde_json;
+    use super::*;
+
+    #[test]
+    fn it_deserializes_an_animation() {
+        let data = r#"{
+    "channels": [
+        {
+            "sampler": "a_sampler",
+            "target": {
+                "id": "node_id",
+                "path": "rotation",
+                "extensions": {
+                    "extension_name": {
+                        "extension specific": "value"
+                    }
+                },
+                "extras": {
+                    "Application specific": "The extra object can contain any properties."
+                }
+            },
+            "extensions": {
+                "extension_name": {
+                    "extension specific": "value"
+                }
+            },
+            "extras": {
+                "Application specific": "The extra object can contain any properties."
+            }
+        }
+    ],
+    "name": "user-defined animation name",
+    "parameters": {
+        "TIME": "time_accessor",
+        "rotation": "rotation_accessor"
+    },
+    "samplers": {
+        "a_sampler": {
+            "input": "TIME",
+            "interpolation": "LINEAR",
+            "output": "rotation",
+            "extensions": {
+                "extension_name": {
+                    "extension specific": "value"
+                }
+            },
+            "extras": {
+                "Application specific": "The extra object can contain any properties."
+            }
+        }
+    },
+    "extensions": {
+        "extension_name": {
+            "extension specific": "value"
+        }
+    },
+    "extras": {
+        "Application specific": "The extra object can contain any properties."
+    }
+}"#;
+        let animation: Animation = serde_json::from_str(data).unwrap();
+
+        assert_eq!("user-defined animation name", animation.name.unwrap());
+        assert_eq!(2, animation.parameters.len());
+    }
+}
