ddec44ec1f40a550a90af00ebf0f32bcbb8f3296
David Harvey-Macaulay <alteous@outlook.com>
2017-03-24 14:06:22 +0000

    Added tests for loading the 2.0 sample models

---
+++ tests/import_v2.rs
@@ -0,0 +1,22 @@
+
+extern crate gltf;
+
+use gltf::Generic::V2;
+
+#[test]
+fn import_v2() {
+    let assets = [
+        // These are currently the only available 2.0 compliant sample assets
+        "glTF-Sample-Models/2.0/Corset/glTF/Corset.gltf",
+        "glTF-Sample-Models/2.0/BoomBox/glTF/BoomBox.gltf",
+        "glTF-Sample-Models/2.0/Lantern/glTF/Lantern.gltf",
+    ];
+    for asset in &assets {
+        match gltf::import(asset) {
+            Ok(V2(_)) => {},
+            Ok(_) => { println!("import() detected wrong version"); panic!() },
+            Err(err) => { println!("{:?}", err); panic!() },
+        }
+    }
+}
+
