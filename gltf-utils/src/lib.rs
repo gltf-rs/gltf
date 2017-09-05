4a06ec862d4c847f7dc1bcaec9393d03bd014b1c
alteous <alteous@outlook.com>
2017-09-05 13:39:23 +0100

    Remove failing doc test

--- gltf-utils/src/lib.rs
+++ gltf-utils/src/lib.rs
@@ -52,45 +52,6 @@ pub trait Source: fmt::Debug {
 }
 
 /// Extra methods for working with `gltf::Primitive`.
-///
-/// # Examples
-///
-/// Collecting the indices of a primitive into a `Vec`.
-///
-/// ```rust
-/// # extern crate gltf;
-/// # extern crate gltf_utils;
-/// # use gltf::Gltf;
-/// # use gltf_utils::Source;
-/// # use std::{fs, io};
-/// # fn run() -> Result<(), Box<std::error::Error>> {
-/// # let path = "../examples/Box.gltf";
-/// # let file = fs::File::open(path)?;
-/// # let gltf = Gltf::from_reader(io::BufReader::new(file))?.validate_minimally()?;
-/// /// Contains the single buffer necessary to render the box model.
-/// #[derive(Debug)]
-/// struct BoxData(&'static [u8]);
-/// impl Source for BoxData {
-///     fn source_buffer(&self, buffer: &gltf::Buffer) -> &[u8] {
-///         assert_eq!(0, buffer.index());
-///         self.0   
-///     }
-/// }
-///
-/// let box_data = BoxData(include_bytes!("../examples/Box0.bin"));
-/// let mesh = gltf.meshes().nth(0).unwrap();
-/// let primitive = mesh.primitives().nth(0).unwrap();
-/// 
-/// use gltf_utils::PrimitiveIterators;
-/// let iter = primitive.indices_u32(&box_data).unwrap();
-/// let indices: Vec<u32> = iter.collect();
-/// # let _ = indices;
-/// # Ok(())
-/// # }
-/// # fn main() {
-/// #    let _ = run().expect("No runtime errors");
-/// # }
-/// ```
 pub trait PrimitiveIterators<'a> {
     /// Visits the vertex positions of a primitive.
     fn positions<S>(&'a self, source: &'a S) -> Option<Positions<'a>>
