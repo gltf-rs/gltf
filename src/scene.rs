0b85f2a1362d8c5fb255bcecece52dda72e61223
David Harvey-Macaulay <alteous@outlook.com>
2017-09-08 14:52:46 +0100

    Fix Transform::decomposed and Gltf::default_scene

--- src/scene.rs
+++ src/scene.rs
@@ -17,7 +17,6 @@ use {Camera, Gltf, Mesh, Skin};
 type Matrix3 = cgmath::Matrix3<f32>;
 type Matrix4 = cgmath::Matrix4<f32>;
 type Quaternion = cgmath::Quaternion<f32>;
-type Vector3 = cgmath::Vector3<f32>;
 
 /// 4x4 identity matrix.
 const IDENTITY: [f32; 16] = {
@@ -57,47 +56,36 @@ impl Transform {
     pub fn matrix(self) -> [[f32; 4]; 4] {
         match self {
             Transform::Matrix { matrix } => matrix,
-            Transform::Decomposed { translation, rotation, scale } => {
-                let t = Matrix4::from_translation(translation.into());
-                let r = Matrix4::from(Quaternion::new(rotation[3], rotation[0], rotation[1], rotation[2]));
-                let s = Matrix4::from_nonuniform_scale(scale[0], scale[1], scale[2]);
+            Transform::Decomposed { translation: t, rotation: r, scale: s } => {
+                let t = Matrix4::from_translation(t.into());
+                let r = Matrix4::from(Quaternion::new(r[3], r[0], r[1], r[2]));
+                let s = Matrix4::from_nonuniform_scale(s[0], s[1], s[2]);
                 (t * r * s).into()
             },
         }
     }
 
-    /// Returns the decomposed representation of this transform.
+    /// Returns a decomposed representation of this transform.
     ///
     /// If the transform is `Matrix`, then the decomposition is extracted from the
     /// matrix.
     pub fn decomposed(self) -> ([f32; 3], [f32; 4], [f32; 3]) {
         match self {
-            Transform::Matrix { matrix: mut m } => {
-                let translation = [
-                    mem::replace(&mut m[3][0], 0.0),
-                    mem::replace(&mut m[3][1], 0.0),
-                    mem::replace(&mut m[3][2], 0.0),
-                ];
-                let sx = Vector3::new(m[0][0], m[0][1], m[0][2]).magnitude();
-                m[0][0] /= sx;
-                m[0][1] /= sx;
-                m[0][2] /= sx;
-                let sy = Vector3::new(m[1][0], m[1][1], m[1][2]).magnitude();
-                m[1][0] /= sy;
-                m[1][1] /= sy;
-                m[1][2] /= sy;
-                let sz = Vector3::new(m[2][0], m[2][1], m[2][2]).magnitude();
-                m[2][0] /= sz;
-                m[2][1] /= sz;
-                m[2][2] /= sz;
-                let scale = [sx, sy, sz];
-                let r = Quaternion::from(
-                    Matrix3::new(
-                        m[0][0], m[0][1], m[0][2],
-                        m[1][0], m[1][1], m[1][2],
-                        m[2][0], m[2][1], m[2][2],
-                    ),
+            Transform::Matrix { matrix: m } => {
+                let translation = [m[3][0], m[3][1], m[3][2]];
+                let mut i = Matrix3::new(
+                    m[0][0], m[0][1], m[0][2],
+                    m[1][0], m[1][1], m[1][2],
+                    m[2][0], m[2][1], m[2][2],
                 );
+                let sx = i.x.magnitude();
+                let sy = i.y.magnitude();
+                let sz = i.determinant().signum() *  i.z.magnitude();
+                let scale = [sx, sy, sz];
+                i.x /= sx;
+                i.y /= sy;
+                i.z /= sz;
+                let r = Quaternion::from(i);
                 let rotation = [r.v.x, r.v.y, r.v.z, r.s];
                 (translation, rotation, scale)
             },
@@ -344,3 +332,31 @@ impl<'a> Iterator for Children<'a> {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use ::cgmath::Matrix4;
+    use ::scene::Transform;
+
+    fn test_decompose(translation: [f32; 3], rotation: [f32; 4], scale: [f32; 3]) {
+        let matrix = Transform::Decomposed { translation, rotation, scale }.matrix();
+        let (translation, rotation, scale) = Transform::Matrix { matrix }.decomposed();
+        let check = Transform::Decomposed { translation, rotation, scale }.matrix();
+        assert_relative_eq!(Matrix4::from(check), Matrix4::from(matrix));
+    }
+
+    #[test]
+    fn decompose_matrix_with_positive_determinant() {
+        let translation = [1.0, 2.0, 3.0];
+        let rotation = [0.0, 0.0, 0.0, 1.0];
+        let scale = [1.0, 1.0, 1.0];
+        test_decompose(translation, rotation, scale);
+    }
+
+    #[test]
+    fn decompose_matrix_with_negative_determinant() {
+        let translation = [1.0, 2.0, 3.0];
+        let rotation = [0.0, 0.0, 0.0, 1.0];
+        let scale = [1.0, 1.0, -1.0];
+        test_decompose(translation, rotation, scale);
+    }
+}
