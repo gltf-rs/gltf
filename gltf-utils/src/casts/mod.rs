15ac4d14a54abe2098cf1c2328748699a5c776ca
Constantine <shablya24@gmail.com>
2017-11-12 20:16:35 +0200

    #[inline(always)] for internal conversion functions.

--- gltf-utils/src/casts/mod.rs
+++ gltf-utils/src/casts/mod.rs
@@ -15,34 +15,42 @@ pub mod morph_weights;
 /// Casting iterator and accompanying types for `Rotations`.
 pub mod rotations;
 
+#[inline(always)]
 fn norm_i8_as_f32(x: i8) -> f32 {
     (x as f32 * (1.0 / i8::max_value() as f32)).max(-1.0)
 }
 
+#[inline(always)]
 fn norm_u8_as_u16(x: u8) -> u16 {
     x as u16 * 0x100
 }
 
+#[inline(always)]
 fn norm_u8_as_f32(x: u8) -> f32 {
     x as f32 * (1.0 / u8::max_value() as f32)
 }
 
+#[inline(always)]
 fn norm_i16_as_f32(x: i16) -> f32 {
     (x as f32 * (1.0 / i16::max_value() as f32)).max(-1.0)
 }
 
+#[inline(always)]
 fn norm_u16_as_u8(x: u16) -> u8 {
     (x / 0x100) as u8
 }
 
+#[inline(always)]
 fn norm_u16_as_f32(x: u16) -> f32 {
     x as f32 * (1.0 / u16::max_value() as f32)
 }
 
+#[inline(always)]
 fn norm_f32_as_u8(x: f32) -> u8 {
     (x * u8::max_value() as f32) as u8
 }
 
+#[inline(always)]
 fn norm_f32_as_u16(x: f32) -> u16 {
     (x * u16::max_value() as f32) as u16
 }
