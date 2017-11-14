47bc560fc9305eb05df4e94a55d2f0c29bece3c6
Constantine <shablya24@gmail.com>
2017-11-14 20:33:56 +0200

    Fix potentially invalid casts (also current potential UB hole).
    Currently, out-of-range float->int casts are UB (in safe Rust!) which is obviously not good, though, current proposed fix is to make float->int cast saturate (see https://github.com/rust-lang/rust/issues/10184).  This fix adds bunch of fail-safes for potentially out of bound values.  This code does not cover NaN, though, NaN to int will become 0, when the behaviour becomes well-defined.

--- gltf-utils/src/casts/mod.rs
+++ gltf-utils/src/casts/mod.rs
@@ -17,7 +17,7 @@ pub mod rotations;
 
 #[inline]
 fn norm_i8_as_f32(x: i8) -> f32 {
-    (x as f32 * (1.0 / i8::max_value() as f32)).max(-1.0)
+    (x as f32 * (i8::max_value() as f32).recip()).max(-1.0)
 }
 
 #[inline]
@@ -27,12 +27,12 @@ fn norm_u8_as_u16(x: u8) -> u16 {
 
 #[inline]
 fn norm_u8_as_f32(x: u8) -> f32 {
-    x as f32 * (1.0 / u8::max_value() as f32)
+    x as f32 * (u8::max_value() as f32).recip()
 }
 
 #[inline]
 fn norm_i16_as_f32(x: i16) -> f32 {
-    (x as f32 * (1.0 / i16::max_value() as f32)).max(-1.0)
+    (x as f32 * (i16::max_value() as f32).recip()).max(-1.0)
 }
 
 #[inline]
@@ -42,15 +42,19 @@ fn norm_u16_as_u8(x: u16) -> u8 {
 
 #[inline]
 fn norm_u16_as_f32(x: u16) -> f32 {
-    x as f32 * (1.0 / u16::max_value() as f32)
+    x as f32 * (u16::max_value() as f32).recip()
 }
 
 #[inline]
 fn norm_f32_as_u8(x: f32) -> u8 {
-    (x * u8::max_value() as f32) as u8
+    let hi = u8::max_value() as f32;
+    let lo = 0.0;
+    (x * hi).max(lo).min(hi) as u8
 }
 
 #[inline]
 fn norm_f32_as_u16(x: f32) -> u16 {
-    (x * u16::max_value() as f32) as u16
+    let hi = u16::max_value() as f32;
+    let lo = 0.0;
+    (x * hi).max(lo).min(hi) as u16
 }
