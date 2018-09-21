ea0769221dc9f7ce2e252f2c076815b14f1b6cd9
David Harvey-Macaulay <alteous@outlook.com>
2018-09-21 20:01:36 +0100

    Expose Extras as RawValue

--- gltf-json/src/extras.rs
+++ gltf-json/src/extras.rs
@@ -1,10 +1,15 @@
 use std::fmt;
 
-pub use serde_json::Value;
+#[cfg(feature = "extras")]
+use ::{Path, Root};
+#[cfg(feature = "extras")]
+pub use serde_json::value::RawValue;
+#[cfg(feature = "extras")]
+use validation::{Error, Validate};
 
 /// Data type of the `extras` attribute on all glTF objects.
 #[cfg(feature = "extras")]
-pub type Extras = Option<Value>;
+pub type Extras = Option<::std::boxed::Box<RawValue>>;
 
 /// Data type of the `extras` attribute on all glTF objects.
 #[cfg(not(feature = "extras"))]
@@ -26,5 +31,20 @@ impl fmt::Debug for Void {
 impl fmt::Display for Void {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{{}}")
+    }
+}
+
+#[cfg(feature = "extras")]
+impl Validate for ::std::boxed::Box<RawValue> {
+    fn validate_minimally<P, R>(&self, _: &Root, _: P, _: &mut R)
+        where P: Fn() -> Path, R: FnMut(&Fn() -> Path, Error)
+    {
+        // nop
+    }
+
+    fn validate_completely<P, R>(&self, _: &Root, _: P, _: &mut R)
+        where P: Fn() -> Path, R: FnMut(&Fn() -> Path, Error)
+    {
+        // nop
     }
 }
