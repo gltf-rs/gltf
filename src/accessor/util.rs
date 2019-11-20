e5e674c8c8e03a0844b6eb1111f6f118e91c9715
Aaron Loucks <aloucks@cofront.net>
2019-11-19 21:23:32 -0500

    Implement ExactSizeIterator for SparseIter
    Added size_hint to SparseIter based on feedback and added all
    ItemIter Iterator overloads to Iter as well.

--- src/accessor/util.rs
+++ src/accessor/util.rs
@@ -33,16 +33,37 @@ impl<'a, T: Item> Iterator for Iter<'a, T> {
             &mut Iter::Sparse(ref mut iter) => iter.next(),
         }
     }
-}
 
-impl<'a, T: Item> ExactSizeIterator for Iter<'a, T> {
-    fn len(&self) -> usize {
+    fn nth(&mut self, nth: usize) -> Option<Self::Item> {
         match self {
-            &Iter::Standard(ref iter) => iter.len(),
-            &Iter::Sparse(ref iter) => iter.values.len(),
+            &mut Iter::Standard(ref mut iter) => iter.nth(nth),
+            &mut Iter::Sparse(ref mut iter) => iter.nth(nth),
+        }
+    }
+
+    fn last(self) -> Option<Self::Item> {
+        match self {
+            Iter::Standard(iter) => iter.last(),
+            Iter::Sparse(iter) => iter.last(),
+        }
+    }
+
+    fn count(self) -> usize {
+        match self {
+            Iter::Standard(iter) => iter.count(),
+            Iter::Sparse(iter) => iter.count(),
+        }
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        match self {
+            &Iter::Standard(ref iter) => iter.size_hint(),
+            &Iter::Sparse(ref iter) => iter.size_hint(),
         }
     }
 }
+
+impl<'a, T: Item> ExactSizeIterator for Iter<'a, T> {}
 
 /// Iterator over indices of sparse accessor.
 #[derive(Clone, Debug)]
@@ -123,7 +144,14 @@ impl<'a, T: Item> Iterator for SparseIter<'a, T> {
 
         Some(next_value)
     }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let hint = self.values.len() - self.counter as usize;
+        (hint, Some(hint))
+    }
 }
+
+impl<'a, T: Item> ExactSizeIterator for SparseIter<'a, T> {}
 
 /// Represents items that can be read by an [`Accessor`].
 ///
