01b04051fe7b90d2d0ece0c3ee2c6fcb4b09d54c
David Harvey-Macaulay <david@harvey-macaulay.com>
2024-04-22 15:40:08 +0100

    Fail validation for unsupported required extensions

--- tests/import_sanity_check.rs
+++ tests/import_sanity_check.rs
@@ -1,22 +1,40 @@
 use std::error::Error as StdError;
 use std::{fs, path};
 
-const SAMPLE_MODELS_DIRECTORY_PATH: &str = "glTF-Sample-Models/2.0";
+const SAMPLE_MODELS_DIRECTORY_PATH: &str = "glTF-Sample-Assets/Models";
 
-fn sanity_check(
-    document: &gltf::Document,
-    buffer_data: &[gltf::buffer::Data],
-    image_data: &[gltf::image::Data],
+fn check_import_result(
+    result: gltf::Result<(
+        gltf::Document,
+        Vec<gltf::buffer::Data>,
+        Vec<gltf::image::Data>,
+    )>,
 ) {
-    // Check buffers.
-    assert_eq!(document.buffers().len(), buffer_data.len());
+    use gltf::json::validation::Error;
+    match result {
+        Err(gltf::Error::Validation(errors)) => {
+            assert!(errors
+                .iter()
+                .all(|(_path, error)| *error == Error::Unsupported));
+            println!("skipped");
+        }
+        Err(otherwise) => {
+            panic!("{otherwise:#?}");
+        }
+        Ok((document, buffer_data, image_data)) => {
+            // Check buffers.
+            assert_eq!(document.buffers().len(), buffer_data.len());
 
-    for (buf, data) in document.buffers().zip(buffer_data.iter()) {
-        assert_eq!((buf.length() + 3) & !3, data.0.len())
+            for (buf, data) in document.buffers().zip(buffer_data.iter()) {
+                assert!((buf.length() + 3) & !3 <= data.0.len())
+            }
+
+            // Check images.
+            assert_eq!(document.images().len(), image_data.len());
+
+            println!("ok");
+        }
     }
-
-    // Check images.
-    assert_eq!(document.images().len(), image_data.len())
 }
 
 fn run() -> Result<(), Box<dyn StdError>> {
@@ -31,30 +49,24 @@ fn run() -> Result<(), Box<dyn StdError>> {
                 let mut gltf_path = entry_path.join("glTF").join(file_name);
                 gltf_path.set_extension("gltf");
                 if gltf_path.exists() {
-                    print!("{:?}: ", gltf_path);
-                    let result = gltf::import(&gltf_path)?;
-                    sanity_check(&result.0, &result.1, &result.2);
-                    println!("ok");
+                    print!("{}: ", gltf_path.display());
+                    check_import_result(gltf::import(&gltf_path));
                 }
 
                 // Import standard glTF with embedded buffer and image data.
                 let mut gle_path = entry_path.join("glTF-Embedded").join(file_name);
                 gle_path.set_extension("gltf");
                 if gle_path.exists() {
-                    print!("{:?}: ", gle_path);
-                    let result = gltf::import(&gle_path)?;
-                    sanity_check(&result.0, &result.1, &result.2);
-                    println!("ok");
+                    print!("{}: ", gle_path.display());
+                    check_import_result(gltf::import(&gle_path));
                 }
 
                 // Import binary glTF.
                 let mut glb_path = entry_path.join("glTF-Binary").join(file_name);
                 glb_path.set_extension("glb");
                 if glb_path.exists() {
-                    print!("{:?}: ", glb_path);
-                    let result = gltf::import(&glb_path)?;
-                    sanity_check(&result.0, &result.1, &result.2);
-                    println!("ok");
+                    print!("{}: ", glb_path.display());
+                    check_import_result(gltf::import(&glb_path));
                 }
             }
         }
@@ -66,20 +78,18 @@ fn run() -> Result<(), Box<dyn StdError>> {
 /// Test a file with a sparse accessor with no buffer view
 fn sparse_accessor_without_buffer_view_test() -> Result<(), Box<dyn StdError>> {
     let glb_path = path::Path::new("tests/box_sparse.glb");
-    print!("{:?}: ", glb_path);
-    let result = gltf::import(glb_path)?;
-    sanity_check(&result.0, &result.1, &result.2);
-    println!("ok");
+    print!("{}: ", glb_path.display());
+    check_import_result(gltf::import(glb_path));
 
     let gltf_path = path::Path::new("tests/box_sparse.gltf");
-    print!("{:?}: ", gltf_path);
-    let result = gltf::import(gltf_path)?;
-    sanity_check(&result.0, &result.1, &result.2);
-    println!("ok");
+    print!("{}: ", gltf_path.display());
+    check_import_result(gltf::import(gltf_path));
     Ok(())
 }
 
 #[test]
-fn import_sanity_check() {
-    assert!(run().is_ok());
+fn import_sample_models() {
+    if let Err(error) = run() {
+        panic!("import failed: {:?}", error);
+    }
 }
